52a925743ba216c958665ee8e6361b471d023ca7
Step1hen <samponsah126@gmail.com>
2024-02-14 21:43:08 -0800

    api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,34 +1,16 @@
 #!/usr/bin/python3
-"""
-Module to query the Reddit API
-"""
+"""Function to query subscribers on a given Reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    Queries the Reddit API and returns the number of subscribers for a given
-    subreddit.
-    """
-    if subreddit is None or type(subreddit) is not str:
-        return 0
-
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-        'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
-                       '(by /u/AmineLamuadni)')
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
-
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            return 0
-        if not response.text:
-            return 0
-        return response.json().get("data", {}).get("subscribers", 0)
-    except requests.RequestException:
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
         return 0
-
-
-if __name__ == "__main__":
-    pass
+    results = response.json().get("data")
+    return results.get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
 """
-Module to query the Reddit API for the top ten hot posts of a subreddit
+Function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
 """
+
 import requests
 
 
 def top_ten(subreddit):
     """
-    Prints the titles of the first 10 hot posts listed for a given subreddit.
+    Function that queries the Reddit API
+    - If not a valid subreddit, print None.
     """
-    if subreddit is None or type(subreddit) is not str:
-        print("None")
-        return
+    req = requests.get(
+        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+        headers={"User-Agent": "Custom"},
+        params={"limit": 10},
+    )
 
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        'User-Agent': ('Python:alx-system_engineering-devops:v1.0 '
-                       '(by /u/AmineLamuadni)')
-    }
-    params = {
-        "limit": 10
-    }
-
-    try:
-        response = requests.get(url, headers=headers, params=params,
-                                allow_redirects=False)
-        if response.status_code != 200:
-            print("None")
-            return
-        if not response.text:
-            print("None")
-            return
-        posts = response.json().get("data", {}).get("children", [])
-        for post in posts:
-            print(post.get("data", {}).get("title"))
-    except requests.RequestException:
-        print("None")
-
-
-if __name__ == "__main__":
-    pass
+    if req.status_code == 200:
+        for get_data in req.json().get("data").get("children"):
+            dat = get_data.get("data")
+            title = dat.get("title")
+            print(title)
+    else:
+        print(None)
